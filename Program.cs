8bc3a5635408f70582c6aa2ef0022d97a6b46a43
Earpengai <pengai.ear@gmail.com>
2024-06-25 23:51:55 +0700

    Register ContactService to DI

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using GrpcDemo;
 using GrpcDemo.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<ContactService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
